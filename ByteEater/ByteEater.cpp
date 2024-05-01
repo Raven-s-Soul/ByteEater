ba943e8083fd94ddc318c2c731ecb3c02ba5c25e
Sario Tedesco <sariotedesco@gmail.com>
2024-05-01 02:03:26 +0200

    Aggiungere i file di progetto.

---
+++ ByteEater/ByteEater.cpp
@@ -0,0 +1,12 @@
+﻿// ByteEater.cpp: definisce il punto di ingresso dell'applicazione.
+//
+
+#include "ByteEater.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
